ce6709d0d33cb8966dad84bf3757bd539a66fb6e
Diego Colombo <dicolomb@microsoft.com>
2025-10-03 19:38:16 +0100

    Removes JSDOM setup
    Eliminates unused JSDOM setup and global variable assignments.
    Simplifies testing configuration by setting up a new test path.
    Makes the project easier to maintain by removing unnecessary code.

--- wallaby.js
+++ wallaby.js
@@ -1,25 +1,6 @@
 module.exports = function (wallaby) {
   process.chdir('packages/knowledge-network');
   
-  // Set up JSDOM globals immediately
-  const { JSDOM } = require('jsdom');
-  const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>', {
-    url: 'http://localhost',
-    pretendToBeVisual: true,
-    resources: 'usable'
-  });
-
-  // Set up DOM globals before returning config
-  global.window = dom.window;
-  global.document = dom.window.document;
-  global.navigator = dom.window.navigator;
-  global.HTMLElement = dom.window.HTMLElement;
-  global.SVGElement = dom.window.SVGElement;
-  global.Element = dom.window.Element;
-  global.Node = dom.window.Node;
-  global.DocumentFragment = dom.window.DocumentFragment;
-  global.HTMLDivElement = dom.window.HTMLDivElement;
-
   return {
     files: [
       'src/**/*.ts',
@@ -28,7 +9,7 @@ module.exports = function (wallaby) {
     ],
     
     tests: [
-      'src/**/*.test.ts'
+      'tests/**/*.test.ts'
     ],
 
     env: {
